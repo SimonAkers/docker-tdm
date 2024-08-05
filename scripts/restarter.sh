152844975e1b0501d81d97e4396feac2dd83f51d
SimonAkers <hayesakers@gmail.com>
2024-08-05 00:02:13 -0400

    Change docker restart command.

--- scripts/restarter.sh
+++ scripts/restarter.sh
@@ -3,6 +3,6 @@
 echo "$(date +[%Y-%m-%d\ %H:%M:%S]) Service started"
 while true; do
 	sleep 21600
-    docker compose restart docker-tdm
+    docker restart docker-tdm
     echo "$(date +[%Y-%m-%d\ %H:%M:%S]) Restart complete"
 done
