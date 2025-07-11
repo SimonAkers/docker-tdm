d3eb31d0150f2a4b0048c40194ec87bf65195e4a
Shayes <hayesakers@gmail.com>
2025-07-11 11:27:18 -0400

    Update to use noVNC.

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,6 +2,10 @@
 
 git -C TwitchDropsMiner pull || git clone $TDM_REPO TwitchDropsMiner
 
+if [ ! -d "./noVNC" ]; then
+    git clone --branch v1.5.0 --single-branch https://github.com/novnc/noVNC.git noVNC
+fi
+
 rm -f /tmp/.X19-lock
 rm -f /tmp/.X11-unix/X19
 
@@ -11,12 +15,15 @@ Xvfb $DISPLAY -screen 0 1600x900x16 &
 
 fluxbox > /dev/null 2>&1 &
 
-x11vnc -passwd $VNCPASS -display $DISPLAY -bg -forever -quiet || exit 1
+#x11vnc -passwd $VNCPASS -display $DISPLAY -bg -forever -quiet || exit 1
+x11vnc -display $DISPLAY -bg -forever -quiet || exit 1
 
 python3 ./TwitchDropsMiner/main.py &
 
 window_id=$(xdotool search --sync --name "Twitch")
 #xdotool windowsize $window_id 100% 100%
 wmctrl -ir $window_id -b add,maximized_vert,maximized_horz
+
+./noVNC/utils/novnc_proxy --vnc localhost:5900
 
 wait
